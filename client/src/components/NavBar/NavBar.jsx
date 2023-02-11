ee00f34c9bc6a8aa3aba96d61da89f1f1d5f0bd0
Jose Miguel Perea <josemiguelperea205@gmail.com>
2023-02-10 22:44:10 -0500

    Complete NavBar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -19,7 +19,32 @@ const NavBar = () => {
     return (
         <div className={styles.containerNavBar}>
             
+            <div>
+                <h1 className={styles.title}>Henry Videogames</h1>
+            </div>
 
+           <div className={styles.containerContactAbout}>
+                <div>
+                    <Link to="#">
+                        <button 
+                            className={styles.contac} 
+                        >
+                            Contact
+                        </button>
+                    </Link>
+                </div>
+                
+                <div>
+                    <Link to="#">
+                        <button 
+                            className={styles.about} 
+                        >
+                            About
+                        </button>
+                    </Link>
+                </div>
+           </div>
+           
         </div>
     )
 };
